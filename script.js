e243a9782f0b243a4e30cb2bdfb8df60694accbe
Your Name <you@example.com>
2025-03-08 15:32:42 +0600

    all done just make a drawer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let total = 0;
 let totalAmount = 0;
+let userInfo=[]
 const seatInformation = []
 const seatSection = document.getElementById("seat-section")
 seatSection.addEventListener("click", event => {
@@ -19,8 +20,14 @@ seatSection.addEventListener("click", event => {
         total++
         changeInnerText("total-seats", total);
         totalAmount += 550
-        changeInnerText("total-amount", totalAmount)
-        changeInnerText("grand-price", totalAmount)
+        changeInnerText("total-amount", totalAmount);
+        changeInnerText("grand-price", totalAmount);
+        accesButton.setAttribute('disabled', true);
+        const leftSeat=getElementByIdText("left-seat");
+        let left=Math.max(0,leftSeat-1)
+
+        changeInnerText("left-seat", left);
+
     }
 });
 const applyBtn = document.getElementById("apply-btn");
@@ -29,7 +36,34 @@ applyBtn.addEventListener("click", event => {
     const accessBtn = document.getElementById("cuppon-apply").value;
     if (accessBtn === "NEW15") {
         const grandPrice = getElementByIdText("grand-price");
-        let lastGrandPrice = grandPrice * 15 / 100;
-        changeInnerText("grand-price", lastGrandPrice)
+        let lastGrandPrice = grandPrice - grandPrice * 15 / 100;
+        changeInnerText("grand-price", lastGrandPrice);
+        applyBtn.setAttribute('disabled', true);
     }
+    else {
+        alert("Plese inter a valid cupon code")
+    }
+});
+document.getElementById("close-card")
+    .addEventListener("click",()=> {
+        const succesCard=document.getElementById("succes-card")
+        succesCard.classList.add("hidden")
+    })
+document.getElementById("submit-btn")
+.addEventListener("submit",(event)=>{
+    event.preventDefault();
+ const passengerName=document.getElementById("passengerName").value;
+ const phoneNumber=document.getElementById("phoneNumber").value;
+ const emailId=document.getElementById("emailId").value;
+ userInfo.push({
+    name:`${passengerName}`,
+    number:`${phoneNumber}`,
+    email:`${emailId}`
+ })
+    
+
+    const succesCard=document.getElementById("succes-card")
+    succesCard.classList.remove("hidden")
+    console.log(userInfo);
+    console.log(userInfo[0].name);
 })
